87968fb18c26d59ecb323a23b996ed52d6a23621
Francisco G <fsalvador23@gmail.com>
2018-06-18 11:37:55 +0200

    Fixed leiden stuff

---
+++ client/column/circleGraph/checkbox/checkbox.js
@@ -0,0 +1,22 @@
+Template.checkbox.helpers({
+  isChecked(id) {
+    var checked = false;
+    var selectedCourses = Session.get("selectedCourses");
+    if (selectedCourses == undefined) return;
+    return selectedCourses[id].checked
+  }
+});
+
+Template.checkbox.events({
+  'click': function (event, template) {
+    var currentState = template.$(".checkbox").attr("c");
+    template.$(".checkbox").attr("c", !currentState);
+    var selectedCourses = Session.get("selectedCourses");
+    if (selectedCourses == undefined) return;
+    selectedCourses[template.$(".checkbox").attr("course")].checked = !currentState;
+    Session.set("selectedCourses", selectedCourses);
+    return;
+
+
+  }
+});
