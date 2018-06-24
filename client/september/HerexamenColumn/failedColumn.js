da24327a0730e2ebd9bca41ff34cd32a0aaefb3e
Francisco G <fsalvador23@gmail.com>
2018-06-24 12:48:51 +0200

    Cleaning code

--- client/september/HerexamenColumn/failedColumn.js
+++ client/september/HerexamenColumn/failedColumn.js
@@ -3,42 +3,16 @@ import { ReactiveVar } from 'meteor/reactive-var'
 Template.failedColumn.created = function() {
   this.failed   = new ReactiveVar(0);
   this.selected = new ReactiveVar(0);
-
   //Checkboxes start all checked at max number of failed courses.
   this.selected.set(Boekingen.find({$and:[{Student: Session.get("student")}, {Score: {$lt: 10}}]}).count());
 };
 
 Template.failedColumn.helpers({
-  /**
-  * @returns [{id,name,grade,semester,credits, columnindex}]
-  */
-  studentCourses() {
-    let semester = this.semester
-    let ownboekingen = Boekingen.find({$and:[{Academischeperiode: semester},{Student: Session.get("student")}]});
-    return ownboekingen;
-  },
-  "cseAvailable":function() {
-    return this.credits !== undefined
-  },
   "eersteCourses":function () {
-    // Boekingen.find({$and:[{Student: Session.get("student")},{ $not: { $gt: 10 } },{Academiejaar: this.Academischeperiode }]});
-    // Boekingen.find({$and:[{Student: Session.get("student")},{ $not: { $gt: 10 } }]});
     return Boekingen.find({$and:[{Academischeperiode: "Eerste Semester"},{Student: Session.get("student")}, {Score: {$lt: 10}}]}).fetch();
   },
   "tweedeCourses":function () {
-    // Boekingen.find({$and:[{Student: Session.get("student")},{ $not: { $gt: 10 } },{Academiejaar: this.Academischeperiode }]});
-    // Boekingen.find({$and:[{Student: Session.get("student")},{ $not: { $gt: 10 } }]});
     return Boekingen.find({$and:[{Academischeperiode: "Tweede Semester"},{Student: Session.get("student")}, {Score: {$lt: 10}}]}).fetch();
-  },
-  "trajectInfo":function () {
-    let semester = this.semester;
-    let columnindex = this.columnindex;
-    let period = this.period;
-    return {
-      semester: semester,
-      columnindex: columnindex,
-      period: period
-    }
   },
   "checked": function () {
     return Template.instance().selected.get();
@@ -53,23 +27,3 @@ Template.failedColumn.events({
     template.selected.set(document.querySelectorAll('paper-checkbox[checked]').length);
   }
 });
-
-/**
-*
-* @param semester
-* @returns score_entry: fieldname of the db
-*/
-let getScoreEntry = function (semester) {
-  var score_entry = 'Score';
-  switch (semester) {
-    case "Eerste Semester":
-    score_entry = 'Scorejanuari';
-    break;
-    case "Tweede Semester":
-    score_entry = 'Scorejuni';
-    break;
-    default:
-    score_entry = 'Score';
-  }
-  return score_entry;
-};
