62540aa81236084ec062063daef6538184562a46
Francisco G <fsalvador23@gmail.com>
2018-06-24 15:19:04 +0200

    Added style to circleGraph

--- client/september/circleGraph/circleGraph.js
+++ client/september/circleGraph/circleGraph.js
@@ -1,34 +1,44 @@
-Template.circleGraph.rendered = function () {
+Template.circleGraph.helpers({
+  "checked":function () {
+    return 60;
+  },
+  "all":function () {
+    return 20;
+  }
+});
 
-  var svg = d3.select(this.find("svg.circleGraph"));
-  var height = 100;
-  var width = 100;
-  var padding = 10;
-
-  svg.attr("width", 100 + padding);
-  svg.attr("height", 100 + padding);
-  svg.append("circle")
-  .attr("cx", (padding + width) / 2)
-  .attr("cy", (padding + height) / 2)
-  .attr("r", width / 2)
-  .attr("fill", "#E8F3F8");
-  svg.append("rect")
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("width", width + padding)
-
-  .attr("fill", "white");
-  svg.append("text")
-  .attr("x", (padding + width) / 2)
-
-  .attr("fill", "#88B458")
-  .attr("text-anchor", "middle")
-  .text("");
-
-  svg.append("circle")
-  .attr("cx", (padding + width) / 2)
-  .attr("cy", (padding + height) / 2)
-  .attr("r", width / 2 + 3)
-  .attr("fill", "none")
-  .attr("stroke", "#C2CBCE");
-}
+// Deprecated ->>>
+// Template.circleGraph.rendered = function () {
+//
+//   let svg = d3.select(this.find(".circleGraph"));
+//   let height = 100;
+//   let width  = 100;
+//
+//   svg.attr("width",  width);
+//   svg.attr("height", height);
+//
+//   svg.append("circle")
+//   .attr("cx", (width)  / 2)
+//   .attr("cy", (height) / 2)
+//   .attr("r", width / 2)
+//   .attr("fill", "#E8F3F8");
+//
+//   svg.append("rect")
+//   .attr("x", 0)
+//   .attr("y", 0)
+//   .attr("width", width)
+//   .attr("fill", "white");
+//
+//   svg.append("text")
+//   .attr("x", (width) / 2)
+//   .attr("fill", "#88B458")
+//   .attr("text-anchor", "middle")
+//   .text("");
+//
+//   svg.append("circle")
+//   .attr("cx", (width) / 2)
+//   .attr("cy", (height) / 2)
+//   .attr("r", width / 2 + 3)
+//   .attr("fill", "none")
+//   .attr("stroke", "#C2CBCE");
+// }
