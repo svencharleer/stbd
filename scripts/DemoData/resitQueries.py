b92868a04e39a9753dce98e147ce564cd50817f3
Unknown <martijn.millecamp@hotmail.com>
2018-06-26 13:24:50 +0200

    Script data

--- scripts/DemoData/resitQueries.py
+++ scripts/DemoData/resitQueries.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from pymongo import MongoClient
 import csv
 import pprint
+import re
+
 
 def find_failed_students(opleiding, boekingen, dictList):
     failed_values = ['00.0', '01.0', '02.0','03.0','04.0','05.0','06.0','07.0','08.0','09.0','NA','#']
@@ -8,9 +12,9 @@ def find_failed_students(opleiding, boekingen, dictList):
     query_januari = { 'Scorejanuari' : {'$in': failed_values}}
     query_juni = { 'Scorejuni' : {'$in': failed_values}}
     query_resits = { '$and' : [ query_januari, query_juni]}
-    query_opleiding = {'$and': [{ "Opleiding": opleiding }, { "Nieuwi/dopleiding": "J" }]}
-
-
+    query_opleiding = make_query_program(opleiding)
+    
+    
     pipeline = [
         {'$match': query_opleiding},
         {'$match': query_resits},
@@ -21,6 +25,8 @@ def find_failed_students(opleiding, boekingen, dictList):
             }
         }
     ]
+
+
     # Filter all students on:
     # Opleiding
     # NIO
@@ -28,7 +34,6 @@ def find_failed_students(opleiding, boekingen, dictList):
     # Historic students
     # Push for each student his scores of september
     agg = list(boekingen.aggregate(pipeline))
-    print len(agg)
     # Remove all students with all resits a '#' or all resits a 'NA'
     removelist = []
     for fail in agg:
@@ -41,13 +46,33 @@ def find_failed_students(opleiding, boekingen, dictList):
                 removelist.append(fail)
     failed_students = [x for x in agg if x not in removelist]  
     # Get number of failed and passed exams
-    print 'Nb of students: ' , len(failed_students)
+    print 'Nb in' , opleiding , ': ' , len(failed_students)
+
     for student in failed_students:
         nb_failed =  len(student['septemberScores'])
         nb_passed_resits = find_nb_passed_resits(student['septemberScores'])
         if (nb_failed < 15):
             localDict = dictList[nb_failed - 1]
             localDict[nb_passed_resits] += 1
+
+def make_query_program(opleiding):
+
+    if (opleiding == "S MA industriële wetenschappen (Brug/Oost)"):
+        regex = re.compile("^S MA I2W", re.IGNORECASE)
+        query_opleiding = {'$and': [{ "Opleiding": regex }, { "Nieuwi/dopleiding": "J" }]}
+
+    elif(opleiding == "S MA biowetenschappen (Geel)"):
+        hulp_query = { '$or': 
+        [ 
+            { "Opleiding": "S MA biowetenschappen: land- en tuinbouwkunde (Geel)" }, 
+            { "Opleiding": "S MA biowetenschappen: voedingsindustrie (Geel)" },
+            { "Opleiding" : "S MA biowetenschappen: voedingsindustrie VT (Geel)"},
+            { "Opleiding" : "S MA biowetenschappen: voedingsindustrie VD (Geel)"}
+        ]}
+        query_opleiding = {'$and': [hulp_query, { "Nieuwi/dopleiding": "J" }]}
+    else:
+        query_opleiding = {'$and': [{ "Opleiding": opleiding }, { "Nieuwi/dopleiding": "J" }]}
+    return query_opleiding
 
 def find_nb_passed_resits(list):
     nb_passed = 0
@@ -58,45 +83,13 @@ def find_nb_passed_resits(list):
     return nb_passed
 
 
-def find_nb_failed_courses(studentId, opleiding, boekingen):
-    # You already know that student is nio and in correct program
-    failed_values = ['00.0', '01.0', '02.0','03.0','04.0','05.0','06.0','07.0','08.0','09.0','NA','#']
-
-    query_januari = { 'Scorejanuari' : {'$in': failed_values}}
-    query_juni = { 'Scorejuni' : {'$in': failed_values}}
-    query_score = { '$and' : [ query_januari, query_juni]}
-    query_opleiding = {'$and': [{ "Opleiding": opleiding }, { "Nieuwi/dopleiding": "J" }]}
-    query_studentId = {'$and': [{ 'Student' : studentId},{ "Academiejaar": { '$ne': "2017-2018" } }]}
-    query_total = { '$and' : [ query_studentId, query_opleiding, query_score]}
-    
-    failed_courses = boekingen.find(query_total)
-    nb_failed_courses = failed_courses.count()
-    return nb_failed_courses
-
-def find_nb_passed_resits_old(studentId, opleiding, boekingen):
-    # You already know that student is nio and in correct program
-    failed_values = ['00.0', '01.0', '02.0','03.0','04.0','05.0','06.0','07.0','08.0','09.0','NA','#']
-    passed_values = ['10.0','11.0','12.0','13.0','14.0','15.0','16.0','17.0','18.0','19.0','20.0']
-
-    query_januari = { 'Scorejanuari' : {'$in': failed_values}}
-    query_juni = { 'Scorejuni' : {'$in': failed_values}}
-    query_september = {'Scoreseptember': {'$in': passed_values}}
-    query_score = { '$and' : [ query_januari, query_juni, query_september]}
-    query_opleiding = {'$and': [{ "Opleiding": opleiding }, { "Nieuwi/dopleiding": "J" }]}
-    query_studentId = {'$and': [{ 'Student' : studentId},{ "Academiejaar": { '$ne': "2017-2018" } }]}
-    query_total = { '$and' : [ query_studentId, query_score, query_opleiding]}
-    
-    passed_resits = boekingen.find(query_total)
-    nb_passed_resits = passed_resits.count()
-    return nb_passed_resits
-
 def percentageDict(dict):
     acc_dict = accumulateDict(dict)
     new_dict = {}
     values =  acc_dict.values()
-    new_dict['nb_failed'] = values[-1]
     total = values[0]
-    for x in range(0,15):
+    nb_values = len(values)
+    for x in range(0,nb_values):
         if (total != 0 ):
             new_dict[x] = int(round(values[x] / float(total) * 100))
         else:
@@ -106,91 +99,114 @@ def percentageDict(dict):
 def accumulateDict(dict):
     new_dict = {}
     values =  dict.values()
-    new_dict['nb_failed'] = values[-1]
     accumulated_values = accumulateRow(values[:-1])
-    for x in range(0,15):
+    nb_values = len(accumulated_values)
+    for x in range(0,nb_values):
         new_dict[x] = accumulated_values[x]
     return new_dict
     
-
+# Used in accumulateDict
 def accumulateRow(num_list):    
     summed=[]
     for index,value in enumerate(num_list):
         summed.append(sum(num_list[index:]))
     return summed
 
+def createTotalRow(opleiding,dictList):
+    totalRow = {}
+    totalRow['program'] =  opleiding
+    nb_values = len(dictList)
+    for x in range(0,nb_values):
+        local_dict = dictList[x]
+        percentage = percentageDict(local_dict)
+        totalRow[x] = percentage.values()
+    return totalRow
+
+def writeProgram(program, boekingen):
+    dictOne = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictTwo = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictThree = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictFour = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictFive = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictSix = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictSeven = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictEight = { 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictNine = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictTen = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictEleven = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictTwelve = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictThirteen = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictFourteen = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+    dictFifteen = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
+
+
+    dictList = [dictOne, dictTwo, dictThree, dictFour, dictFive,
+    dictSix, dictSeven, dictEight, dictNine, dictTen, dictEleven, dictTwelve, dictThirteen, dictFourteen, dictFifteen]    
+
+    find_failed_students(program, boekingen, dictList)
+
+    row = createTotalRow(program,dictList)
+
+    # Write percentage values to csv
+    with open('dataPercentage.csv', 'a') as csvfile:
+        fieldnames = ['program',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(row)
+
+
+
 def main():
     # Connect to localhost mongodb
     client = MongoClient('localhost', 27017)
-    dictOne = {'nb_failed': 1, 0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictTwo = {'nb_failed': 2,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictThree = {'nb_failed': 3,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictFour = {'nb_failed': 4,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictFive = {'nb_failed': 5,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictSix = {'nb_failed': 6,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictSeven = {'nb_failed': 7,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictEight = {'nb_failed': 8,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictNine = {'nb_failed': 9,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictTen = {'nb_failed': 10,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictEleven = {'nb_failed': 11,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictTwelve = {'nb_failed': 12,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictThirteen = {'nb_failed': 13,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-    dictFourteen = {'nb_failed': 14,0:0, 1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0}
-
-    dictList = [dictOne, dictTwo, dictThree, dictFour, dictFive,
-    dictSix, dictSeven, dictEight, dictNine, dictTen, dictEleven, dictTwelve, dictThirteen, dictFourteen]
-
 
     # Get the meteor database
     db = client.Lissa
 
     # # Get the collections
     boekingen = db.boekingen
+    
 
-    # opleiding = "ABA ingenieurswetenschappen (Leuv)"
-    opleiding = "ABA chemie (Leuv)"
+    # Write percentage values to csv
+    with open('dataPercentage.csv', 'a') as csvfile:
+        fieldnames = ['program',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+    programList = ["ABA biochemie en biotechnologie (Leuv)", "ABA biologie (Leuv)", "ABA chemie (Leuv)", "ABA fysica (Leuv)", "ABA geografie (Leuv)", "ABA geologie (Leuv)", "ABA informatica (Leuv)",
+    "ABA wiskunde (Leuv)", "ABA bio-ingenieurswetenschappen (Leuv)", "ABA ingenieurswetenschappen (Leuv)", "ABA ingenieurswetenschappen: architectuur (Leuv)", "ABA biowetenschappen (Geel)", "ABA industriële wetenschappen (Geel)",
+    "ABA industriële wetenschappen (Aals)", "ABA industriële wetenschappen (Diepenbeek)", "ABA industriële wetenschappen (Leuv)", "ABA industriële wetenschappen (Brug/Oost)", "ABA industriële wetenschappen (StKa)", 
+    "ABA industriële wetenschappen (Gent)",  "S MA biowetenschappen (Geel)", "S MA industriële wetenschappen (Geel)", "S MA industriële wetenschappen (Aals)", "S MA industriële wetenschappen (Diepenbeek)", "S MA industriële wetenschappen (Leuv)", 
+    "S MA industriële wetenschappen (Brug/Oost)", "S MA industriële wetenschappen (StKa)", "ABA architectuur (Gent)", "ABA architectuur (Brus)", "ABA interieurarchitectuur (Gent)",  "ABA interieurarchitectuur (Brus)", 
+    "ABA geneeskunde (Leuv)", "ABA tandheelkunde (Leuv)", "ABA biomedische wetenschappen (Leuv)",  "ABA logopedische en audiologische wetenschappen (Leuv)",  "ABA farmaceutische wetenschappen (Leuv)", 
+    "ABA TEW: handelsingenieur (Leuv)", "ABA geschiedenis (Leuv)", "ABA taal- & letterkunde (Leuv)", "S MA verpleegkunde en vroedkunde (Leuv ea)",  "ABA TEW: handelsingenieur beleidsinformatica (Leuv)" ]
+    for program in programList:
+        writeProgram(program, boekingen)
 
     
 
-    # find all students
-    all_nio_students = boekingen.find({ '$and' : [ 
-        { "Opleiding":  opleiding},
-        { "Nieuwi/dopleiding": "J" },
-        { "Academiejaar": { '$ne': "2017-2018" } }
-        ] }).distinct("Student")
-    print "all students " , len(all_nio_students)
+    
 
-    find_failed_students(opleiding, boekingen, dictList)
+    # # Write raw values to csv
+    # with open('data.csv', 'w') as csvfile:
+    #     fieldnames = ['program',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for i in range(0,14):
+    #         writer.writerow(dictList[i])
+    #         print sum(dictList[i].values()) - i -1
 
-    # Write raw values to csv
-    with open('data.csv', 'w') as csvfile:
-        fieldnames = ['nb_failed',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(0,14):
-            writer.writerow(dictList[i])
-            print sum(dictList[i].values()) - i -1
+    # # Write accumulated values to csv
+    # with open('dataAccumulated.csv', 'w') as csvfile:
+    #     fieldnames = ['nb_failed',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for i in range(0,14):
+    #         dictRow = dictList[i]
+    #         accumulatedDictRow = accumulateDict(dictRow)
+    #         percentageDict(dictRow)
+    #         writer.writerow(accumulatedDictRow)
 
-    # Write accumulated values to csv
-    with open('dataAccumulated.csv', 'w') as csvfile:
-        fieldnames = ['nb_failed',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(0,14):
-            dictRow = dictList[i]
-            accumulatedDictRow = accumulateDict(dictRow)
-            percentageDict(dictRow)
-            writer.writerow(accumulatedDictRow)
-
-    # Write percentage values to csv
-    with open('dataPercentage.csv', 'w') as csvfile:
-        fieldnames = ['nb_failed',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(0,14):
-            dictRow = dictList[i]
-            percentageDictRow = percentageDict(dictRow)
-            writer.writerow(percentageDictRow)
+    
 
 
 
